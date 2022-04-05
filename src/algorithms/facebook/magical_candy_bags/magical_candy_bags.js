fa3c6c90329040e1711e3ec53fc1af04cbac80f7
Caleb Adepitan <calebpitan@outlook.com>
2022-04-05 14:22:16 +0100

    Implement a better solution and describe it in comments

--- src/algorithms/facebook/magical_candy_bags/magical_candy_bags.js
+++ src/algorithms/facebook/magical_candy_bags/magical_candy_bags.js
@@ -1,32 +1,38 @@
+import { heapify, insert, remove } from "../../../data_structures/heap/max_heap.js";
 import { PriorityQueue } from "../../../data_structures/priority_queue/priority_queue.js";
 
 /**
- * Count the maximum amount of candy that can be eaten from `n` bags of candies.
+ * Count the maximum amount of candy that can be eaten from `n` bags of candies
+ * in `k` minutes.
  * Each candy refills with half it original number, floored.
+ *
+ * ### Solution
+ *
+ * 1. Max-heapify the array `arr` in `O(n)` time
+ * 2. While `k` minutes is not zero, pick a bag, in `O(log n)`, that has the largest
+ * amount of candy in it, consume all the candies in the bag in one minute and
+ * increase the counter, `output`, by the number of candies in that bag.
+ * 3. Refill the bag with the floored value of half the number of candies that
+ * was previously inside it, in `(log n)` time.
+ * 3. Recuce `k` by the one minute it took to consume one bag of candies.
+ * 4. Go to step (2)
+ * 5. A resultant loop with time complexity `O(k log n)`
+ * 6. A resultant solution with time complexity `O(n + k log n)` and space `O(1)`
  *
  * @param {number[]} arr The array of the amount of candy in each bag of candies
  * @param {number} k The time in minutes we have to eat as much candies
  * @returns {number}
  */
 function maxCandies(arr, k) {
-  // O(max(k, n) * log(n))
   // Write your code here
-  const pq = new PriorityQueue();
   let output = 0;
 
-  // O(n)
-  // A better solution might be to heapify in O(n) rather
-  // than enqueueing in a total of O(n log n)
-  for (let i = 0; i < arr.length; i++) {
-    pq.enqueue(arr[i]); // O(log n)
-  }
+  heapify(arr)
 
-  // O(k)
-  // resultant complexity: O(k log n)
   while (k > 0) {
-    const candy = pq.dequeue(); // O(log n)
-    pq.enqueue(Math.floor(candy / 2)); // O(log n)
+    const candy = remove(arr)
     output += candy;
+    insert(arr, Math.floor(candy / 2))
     k--;
   }
 
