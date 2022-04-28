e79ceb880eb0edad3ed9b4d25b7cfec4277a7ef1
Caleb Adepitan <calebpitan@outlook.com>
2022-04-28 12:21:26 +0100

    Fix edge case of something like: "[}{]"

--- src/algorithms/facebook/balance_bracket/balance_bracket.js
+++ src/algorithms/facebook/balance_bracket/balance_bracket.js
@@ -3,6 +3,15 @@ const brackets = { "{": 1, "[": 2, "(": 3, ")": -3, "]": -2, "}": -1 };
 /** @type {(stack: number[]) => number | undefined} */
 const peek = (stack) => stack[stack.length - 1];
 
+/** @type {(stack: number[]) => boolean} */
+const empty = (stack) => stack.length === 0;
+
+/** @type {(code: number) => boolean} */
+const isPopCode = (code) => Math.sign(code) === -1;
+
+/** @type {(top: number, code: number) => boolean} */
+const balanced = (top, code) => top + code === 0;
+
 /**
  * Checks if the brackets in a string `s` are balanced.
  * @param {string} s The string conatining various brackets, open and/or close.
@@ -10,24 +19,31 @@ const peek = (stack) => stack[stack.length - 1];
  */
 function isBalanced(s) {
   // Write your code here
-  const stack = [];
+    const stack = [];
 
-  for (let i = 0; i < s.length; i++) {
-    const c = /** @type {keyof typeof brackets} */ (s.charAt(i));
-    const code = brackets[c];
-    const top = peek(stack);
+    for (let i = 0; i < s.length; i++) {
+      const c = /** @type {keyof typeof brackets} */ (s.charAt(i));
+      const code = brackets[c];
 
-    if (top) {
-      top + code === 0 ? stack.pop() : stack.push(code);
-      continue;
+      if (empty(stack)) {
+        if (isPopCode(code)) return false;
+        stack.push(code);
+        continue;
+      }
+
+      const top = /** @type {number} */ (peek(stack));
+
+      if (balanced(top, code)) {
+        stack.pop();
+        continue;
+      }
+
+      if (isPopCode(code)) return false;
+
+      stack.push(code);
     }
 
-    if (Math.sign(code) === -1) return false;
-
-    stack.push(code);
-  }
-
-  return stack.length === 0;
+    return stack.length === 0;
 }
 
 export { isBalanced };
