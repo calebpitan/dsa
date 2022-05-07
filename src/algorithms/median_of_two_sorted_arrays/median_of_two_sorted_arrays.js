e2e6cb84f415634005af90c5e6daa65e4f431183
Caleb Adepitan <calebpitan@outlook.com>
2022-05-07 22:13:17 +0100

    Improve solution to be more correct

--- src/algorithms/median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
+++ src/algorithms/median_of_two_sorted_arrays/median_of_two_sorted_arrays.js
@@ -17,7 +17,7 @@
  * position in the array else assign a negative infinity
  * 9. While the index of the offset mid pointer isn't out of bounds pick the element at
  * that position in the array else assign infinity
- * 10. // (Too many edge cases)
+ * 10. 
  *
  * @param {number[]} nums1 One of the two sorted arrays
  * @param {number[]} nums2 The second of the two sorted arrays
@@ -26,50 +26,34 @@
 function findMedianSortedArrays(nums1, nums2) {
   let m = nums1.length;
   let n = nums2.length;
-  let l = 0;
-  let r = m - 1;
   let total = m + n;
   let half = Math.floor(total / 2);
 
-  if (m > n) return findMedianSortedArrays(nums2, nums1);
+  let l = 0;
+  let r = m;
+  let median = 0;
 
-  if (m == 0) {
-    let mid = Math.floor((n - 1) / 2);
-    if (total % 2 == 1) return nums2[mid];
-    return (nums2[mid] + nums2[mid + 1]) / 2;
-  }
+  if (n < m) return findMedianSortedArrays(nums2, nums1);
 
-  if (n == 0) {
-    let mid = Math.floor((m - 1) / 2);
-    if (total % 2 == 1) return nums1[mid];
-    return (nums1[mid] + nums1[mid + 1]) / 2;
-  }
-
-  while (true) {
+  while (l <= r) {
     let mid1 = Math.floor((l + r) / 2);
-    let mid2 = half - 1 - (mid1 + 1);
+    let mid2 = half - mid1;
 
-    let smallMid = mid1 >= 0 ? nums1[mid1] : -Infinity;
-    let smallMidOffset = mid1 + 1 < m ? nums1[mid1 + 1] : Infinity;
+    let left1 = mid1 > 0 ? nums1[mid1 - 1] : -Infinity;
+    let left2 = mid2 > 0 ? nums2[mid2 - 1] : -Infinity;
+    let right1 = mid1 < m ? nums1[mid1] : Infinity;
+    let right2 = mid2 < n ? nums2[mid2] : Infinity;
 
-    let largeMid = mid2 >= 0 ? nums2[mid2] : -Infinity;
-    let largeMidOffset = mid2 + 1 < n ? nums2[mid2 + 1] : Infinity;
-
-    if (smallMid <= largeMidOffset && largeMid <= smallMidOffset) {
-      if (total % 2 == 1) {
-        return Math.min(largeMidOffset, smallMidOffset);
-      }
-      return (
-        (Math.max(largeMid, smallMid) +
-          Math.min(largeMidOffset, smallMidOffset)) /
-        2
-      );
-    } else if (smallMid > largeMidOffset) {
-      r = mid2 - 1;
-    } else if (largeMid > smallMidOffset) {
+    if (left1 <= right2 && left2 <= right1) {
+      if (total % 2 == 1) return Math.min(right1, right2);
+      return (Math.max(left1, left2) + Math.min(right1, right2)) / 2;
+    } else if (left1 > right2) {
+      r = mid1 - 1;
+    } else if (left2 > right1) {
       l = mid1 + 1;
     }
   }
+  return median;
 }
 
 export { findMedianSortedArrays };
