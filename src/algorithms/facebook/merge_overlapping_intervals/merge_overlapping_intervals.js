55fe7a3a41568ad5226624bd6472031187650bf3
Caleb Adepitan <calebpitan@outlook.com>
2022-05-13 09:02:44 +0100

    Create a better and working solution

--- src/algorithms/facebook/merge_overlapping_intervals/merge_overlapping_intervals.js
+++ src/algorithms/facebook/merge_overlapping_intervals/merge_overlapping_intervals.js
@@ -1,3 +1,8 @@
+import {
+  heapify,
+  remove,
+} from "../../../data_structures/heap/min_heap_queue.js";
+
 /**
  * Merge a list of overlapping intervals as one single intervals picking the smallest
  * start interval and the largest end interval, to replace the overlapping ones
@@ -5,50 +10,65 @@
  * @returns {Array<[number, number]>}
  */
 function mergeOverlappingIntervals(intervals) {
-  let mid = Math.floor(intervals.length / 2);
-  if (mid < 1) return intervals;
+  const output = [];
+  heapify(intervals);
 
-  let left = mergeOverlappingIntervals(intervals.slice(0, mid));
-  let right = mergeOverlappingIntervals(intervals.slice(mid));
+  let left = remove(intervals);
+  let right = remove(intervals);
 
-  return merge(left, right);
-}
+  while (left) {
+    if (!right) {
+      output.push(left);
+      break;
+    }
 
-/**
- * Merge algorithm
- * @param {Array<[number, number]>} left The left partition
- * @param {Array<[number, number]>} right The right partition
- * @returns {Array<[number, number]>}
- */
-function merge(left, right) {
-  let l = 0;
-  let r = 0;
-  /** @type {Array<[number, number]>} */
-  let merged = [];
+    let [start1, end1] = left;
+    let [start2, end2] = right;
 
-  while (l < left.length && r < right.length) {
-    let [start1, end1] = left[l];
-    let [start2, end2] = right[r];
-
-    if (start1 < start2 && end1 < start2) {
-      merged.push(left[l++], right[r++]);
+    if (end1 >= start2) {
+      const start = Math.min(start1, start2);
+      const end = Math.max(end1, end2);
+      left = [start, end];
+      right = remove(intervals);
       continue;
     }
 
-    merged.push([Math.min(start1, start2), Math.max(end1, end2)]);
-    l++;
-    r++;
+    output.push(left);
+    left = right;
+    right = remove(intervals);
   }
-
-  while (l < left.length) {
-    merged.push(left[l++]);
-  }
-
-  while (r < right.length) {
-    merged.push(right[r++]);
-  }
-
-  return merged;
+  return output;
 }
 
-export { mergeOverlappingIntervals };
+/**
+ * Merge a list of overlapping intervals as one single intervals picking the smallest
+ * start interval and the largest end interval, to replace the overlapping ones
+ * @param {Array<[number, number]>} intervals A list of overlapping intervals [start, end]
+ * @returns {Array<[number, number]>}
+ */
+function mergeOverlappingIntervals2(intervals) {
+  if (intervals.length === 0) return intervals;
+
+  intervals.sort((a, b) => b[0] - a[0]);
+  
+  let output = [/** @type {[number, number]} */ (intervals.pop())];
+
+  /** @type {<T>(arr: Array<T>) => T} */
+  let peek = (arr) => arr[arr.length - 1];
+
+  while (intervals.length > 0) {
+    let current = /** @type {[number, number]} */ (intervals.pop());
+    let [start1, end1] = peek(output);
+    let [start2, end2] = current;
+
+    if (end1 >= start2) {
+      output[output.length - 1][0] = Math.min(start1, start2);
+      output[output.length - 1][1] = Math.max(end1, end2);
+      continue;
+    }
+    output.push(current);
+  }
+  return output;
+}
+
+export { mergeOverlappingIntervals, mergeOverlappingIntervals2 };
--- src/algorithms/facebook/merge_overlapping_intervals/merge_overlapping_intervals.test.js
+++ src/algorithms/facebook/merge_overlapping_intervals/merge_overlapping_intervals.test.js
@@ -1,4 +1,7 @@
-import { mergeOverlappingIntervals } from "./merge_overlapping_intervals.js";
+import {
+  mergeOverlappingIntervals,
+  mergeOverlappingIntervals2,
+} from "./merge_overlapping_intervals.js";
 
 describe("merge_overlapping_intervals", () => {
   /** @type {Array<[number, number]>} */
@@ -15,6 +18,16 @@ describe("merge_overlapping_intervals", () => {
     [7, 10],
     [11, 19],
   ];
+  /** @type {Array<[number, number]>} */
+  let intervals3 = [
+    [2, 3],
+    [2, 2],
+    [3, 3],
+    [1, 3],
+    [5, 7],
+    [2, 2],
+    [4, 6],
+  ];
 
   let expected1 = [
     [1, 6],
@@ -25,10 +38,22 @@ describe("merge_overlapping_intervals", () => {
     [1, 10],
     [11, 19],
   ];
+  let expected3 = [
+    [1, 3],
+    [4, 7],
+  ];
 
   it("mergeOverlappingIntervals - should merge two intervals left and right that overlap into one single interval", () => {
     expect(mergeOverlappingIntervals([])).toMatchObject([]);
-    expect(mergeOverlappingIntervals(intervals1)).toMatchObject(expected1);
-    expect(mergeOverlappingIntervals(intervals2)).toMatchObject(expected2);
+    expect(mergeOverlappingIntervals(intervals1.slice())).toMatchObject(expected1);
+    expect(mergeOverlappingIntervals(intervals2.slice())).toMatchObject(expected2);
+    expect(mergeOverlappingIntervals(intervals3.slice())).toMatchObject(expected3);
+  });
+
+  it("mergeOverlappingIntervals2 - should merge two intervals left and right that overlap into one single interval", () => {
+    expect(mergeOverlappingIntervals2([])).toMatchObject([]);
+    expect(mergeOverlappingIntervals2(intervals1.slice())).toMatchObject(expected1);
+    expect(mergeOverlappingIntervals2(intervals2.slice())).toMatchObject(expected2);
+    expect(mergeOverlappingIntervals2(intervals3.slice())).toMatchObject(expected3);
   });
 });
